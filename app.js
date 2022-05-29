c2738d053c7cf3f8c6c2bf85d3591013ebad2213
Shubham <htcc58002900@gmail.com>
2022-05-29 17:51:31 +0530

    final project

--- app.js
+++ app.js
@@ -1,38 +1,58 @@
 const express=require("express");
 const bodyParser=require("body-parser")
 const app=express();
-var items=[];
+let items=["Buy Food","Cook Food","Eat Food"];
+let workItem=[];
 
 app.set("view engine","ejs");
 
 app.use(bodyParser.urlencoded({extended:true}));
-
+app.use(express.static("public"));
 app.get("/",function(req,res){
-    var today=new Date();
-    var currentDay=today.getDay();
-    var day="Monday";
-    var message="";
+    let today=new Date();
+    let options = {
+        weekday:"long",
+        day:"numeric",
+        month:"long"
+    };
+    let currentDay=today.toLocaleDateString("en-US",options);
 
-    switch(currentDay){
-        case 0:day="Sunday";message="Yay! Today I will enjoy";break;
-        case 1:day="Monday";message="Ohh No! I have work today";break;
-        case 2:day="Tuesday";message="Ohh No! I have work today";break;
-        case 3:day="Wednesday";message="Ohh No! I have work today";break;
-        case 4:day="Thrusday";message="Ohh No! I have work today";break;
-        case 5:day="Friday";message="Ohh No! I have work today";break;
-        case 6:day="Saturday";message="Yay! Today I will enjoy";break;
-    }
 
-    
-    res.render('list',{itemList:items});
+
+    res.render('list',{itemList:items,listTitle:currentDay});
 })
 
 app.post("/",function(req,res){
     var item=req.body.newItem;
-    console.log(item);
-    items.push(item);
-    res.redirect("/");
+    console.log(req.body.list);
+    if(req.body.list === "Work"){
+        workItem.push(item);
+        res.redirect("/work");
+    }else{
+        items.push(item);
+        res.redirect("/")
+    }
+    
 })
+
+app.get("/work",function(req,res){
+    let today=new Date();
+    let options = {
+        weekday:"long",
+        day:"numeric",
+        month:"long"
+    };
+    let currentDay=today.toLocaleDateString("en-US",options);
+
+
+
+    res.render('list',{itemList:workItem,listTitle:"Work List"});
+})
+
+app.get('/about',function(req,res){
+    res.render("about");
+})
+
 app.listen(3000, function(){
     console.log("Server started on port 3000");
 }) 
